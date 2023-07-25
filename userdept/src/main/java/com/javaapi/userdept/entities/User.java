51a05a872d6789bc93d751c288b9239720c0fa32
Juan Redivo <juanredivo@hotmail.com>
2023-07-25 04:16:05 -0300

    configuração foreign key (department_id)

--- userdept/src/main/java/com/javaapi/userdept/entities/User.java
+++ userdept/src/main/java/com/javaapi/userdept/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,6 +42,8 @@ public class User {
         this.email = email;
     }
 
+    @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department;
 
     public Department getDepartment() {
